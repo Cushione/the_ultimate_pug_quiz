2e5b49d9c8ddbe25d08ff97e538886d9ac90c26e
Cushione <maulerovak@gmail.com>
2023-12-31 15:26:47 +0100

    Add quiz models

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from django.forms import BaseInlineFormSet, forms
 
-# Register your models here.
+import quiz.models as models
+
+
+class AnswerAdminInlineFormset(BaseInlineFormSet):
+
+    def clean(self):
+        super().clean()
+        valid = False
+        for form in self.forms:
+            try:
+                if form.cleaned_data and form.cleaned_data['correct'] is not None:
+                    if form.cleaned_data['correct']:
+                        valid = True
+                        break
+
+            except AttributeError:
+                pass
+        if not valid:
+            raise forms.ValidationError('You must have at least one correct answer')
+
+
+class AnswerAdminInline(admin.TabularInline):
+    model = models.Answer
+    formset = AnswerAdminInlineFormset
+
+
+@admin.register(models.Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerAdminInline]
+
+
+class GameQuestionsAdminInline(admin.TabularInline):
+    model = models.GameQuestions
+    exclude = ('order',)
+    readonly_fields = ('question', 'correct')
+    can_delete = False
+    max_num = 10
+
+
+@admin.register(models.Game)
+class GameAdmin(admin.ModelAdmin):
+    model = models.Game
+    inlines = [GameQuestionsAdminInline]
+    exclude = ('questions', 'uuid')
+
+    def has_add_permission(self, request, obj=None):
+        return models.Question.objects.all().count() >= 10
--- quiz/models.py
+++ quiz/models.py
@@ -1,3 +1,59 @@
-from django.db import models
+import random
 
-# Create your models here.
+from django.db import models
+from shortuuid.django_fields import ShortUUIDField
+
+
+class Question(models.Model):
+    body = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.body
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    body = models.CharField(max_length=120)
+    correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.body
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['question', 'body'], name='unique_answers'),
+        ]
+
+
+class Game(models.Model):
+    uuid = ShortUUIDField(length=8)
+    questions = models.ManyToManyField(Question, through='GameQuestions')
+
+    def __str__(self):
+        return self.uuid
+
+    def save(self, *args, **kwargs):
+        super(Game, self).save(*args, **kwargs)
+        if not self.questions.exists():
+            questions = list(Question.objects.all())
+            random_questions = random.sample(questions, 10)
+            order = 0
+            for question in random_questions:
+                game_question = GameQuestions(game=self, question=question, order=order)
+                game_question.save()
+                order += 1
+
+
+class GameQuestions(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    order = models.IntegerField()
+    correct = models.BooleanField(null=True)
+
+    def __str__(self):
+        return 'Questions ' + str(self.order)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['game', 'question', 'order'], name='unique_order'),
+        ]
