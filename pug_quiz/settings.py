71ef56b04868d5352a80824e9937f7f082ac5955
Cushione <maulerovak@gmail.com>
2023-12-31 16:47:50 +0000

    Remove reload middleware

--- pug_quiz/settings.py
+++ pug_quiz/settings.py
@@ -64,7 +64,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'pug_quiz.urls'
@@ -92,10 +91,10 @@ WSGI_APPLICATION = 'pug_quiz.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # Password validation
