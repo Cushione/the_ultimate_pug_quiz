3cbded177c6a74d22c6aa4a2211ab67e9006d83b
Cushione <maulerovak@gmail.com>
2023-12-31 15:42:11 +0100

    Remove db service from config

--- pug_quiz/settings.py
+++ pug_quiz/settings.py
@@ -92,10 +92,10 @@ WSGI_APPLICATION = 'pug_quiz.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/pug_quiz',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
